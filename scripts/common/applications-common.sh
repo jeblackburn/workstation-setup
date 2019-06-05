578c7bbf1e56e488333993e0fee6fed9654c86fa
Jon Blackburn <jb@jonathanblackburn.net>
2019-06-05 14:47:09 -0500

    Add VS Code configuration.

--- scripts/common/applications-common.sh
+++ scripts/common/applications-common.sh
@@ -33,4 +33,29 @@ brew cask install slack
 brew cask install textmate
 brew cask install jetbrains-toolbox --force # guard against pre-installed jetbrains-toolbox
 brew cask install visual-studio-code
+echo
+echo "Installing the VS Code extensions and snippets."
+cp files/vscode/java.json ~/Library/Application\ Support/Code/User/snippets/
+code --install-extension bungcip.better-toml
+code --install-extension formulahendry.code-runner
+code --install-extension johnpapa.vscode-peacock
+code --install-extension k--kato.intellij-idea-keybindings
+code --install-extension karigari.chat
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vsliveshare.vsliveshare-audio
+code --install-extension ms-vsliveshare.vsliveshare-pack
+code --install-extension naco-siren.gradle-language
+code --install-extension Pivotal.vscode-boot-dev-pack
+code --install-extension Pivotal.vscode-concourse
+code --install-extension Pivotal.vscode-manifest-yaml
+code --install-extension Pivotal.vscode-spring-boot
+code --install-extension redhat.java
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
+code --install-extension vscjava.vscode-spring-boot-dashboard
+code --install-extension vscjava.vscode-spring-initializr
 set -e
